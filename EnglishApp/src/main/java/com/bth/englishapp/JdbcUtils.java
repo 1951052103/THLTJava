8b89efbbb960ba64f5136796cbe67154109fa9e7
Name <1951052103long@ou.edu.vn>
2022-07-02 22:26:47 +0700

    save

---
+++ EnglishApp/src/main/java/com/bth/englishapp/JdbcUtils.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.bth.englishapp;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author admin
+ */
+public class JdbcUtils {
+    private static Connection conn;
+    
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(JdbcUtils.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * @return the conn
+     */
+    public static Connection getConn() throws SQLException {
+        conn = DriverManager.getConnection("jdbc:mysql://localhost/englishdb", "root", "12345678");
+        
+        return conn;
+    }
+}
